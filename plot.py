da7be86f747c27f500446fd92fd0078b8ad783cc
Chu-Ya Yang <graceyang406@tlogin-0502.cluster>
2019-04-19 12:47:19 -0500

    update plot.py

--- plot.py
+++ plot.py
@@ -4,10 +4,12 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 parser = argparse.ArgumentParser(description='plot')
-parser.add_argument('--state', type=string, help='path of state')
+parser.add_argument('--state', type=str, help='path of state')
+parser.add_argument('--suffix', type=str, default='', help='suffix of figure name')
 args = parser.parse_args()
 
-def plot(state):
+def plot(state_path, suffix=''):
+    state = torch.load(state_path, map_location='cpu')
     acc_history = state['acc_history']
     loss_history = state['loss_history']
     epoch = np.arange(1, len(acc_history) + 1)
@@ -16,14 +18,8 @@ def plot(state):
     plt.plot(epoch, acc_history)
     plt.xlabel('epoch')
     plt.ylabel('validation accuracy(%)')
-    fig.savefig('validation_accuracy.png')
+    fig.savefig('validation_accuracy_{}.png'.format(suffix))
     
-    fig = plt.figure()
-    plt.plot(epoch, loss_history)
-    plt.xlabel('epoch')
-    plt.ylabel('loss')
-    fig.savefig('loss.png')
-
 if __name__ == '__main__':
-    plot(torch.load(args.state))
+    plot(args.state, args.suffix)
 
